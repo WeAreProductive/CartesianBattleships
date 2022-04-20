2237bbc1a1f0ecda0ec6f6c879f01baf0cd39fa7
lachezar.gorchev <lachezar.gorchev@bgosoftware.com>
2022-04-20 16:57:48 +0300

    Additional dumps at game end.

--- server/bs_game/log_flow.py
+++ server/bs_game/log_flow.py
@@ -21,8 +21,10 @@ def dumpPlayerMsg(_gameState, cmd, response):
 	if cmd.cmdType == 'e':
 		if _gameState.result == cmd.playerTag:
 			logI(f"{cc.action}Player {cmd.playerTag} confirms end game as a {cc.p_win}winner{cc.action}.{cc.NC}")
+			dumpGameInfo(_gameState, False)
 		else:
 			logI(f"{cc.action}Player {cmd.playerTag} declares {cc.p_defeat}defeat{cc.action}.{cc.NC}")
 			dumpGameplayBoards(_gameState, False)
+			dumpGameplayMoves(_gameState, False)
 
 	logI(f"{cc.sep}===== User message <<< ====={cc.NC}")
