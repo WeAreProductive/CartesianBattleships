e5ec6ac96cc6445a5b2b666f8262bc1457c349eb
lachezar.gorchev <lachezar.gorchev@bgosoftware.com>
2022-05-18 22:11:31 +0300

    Input CLI argument to select player. Interactive menu to select player if not provided.

--- frontend-cli/bin/index.js
+++ frontend-cli/bin/index.js
@@ -7,20 +7,47 @@
 // import inquirer from "inquirer";
 // import TerminalMenu from "simple-terminal-menu";
 
+import inquirer from "inquirer";
+import { defaultWallets } from "../src/connect/wallets"
 import Menu from "../src/menu.js";
 import GamePlay from "../src/game-play.js";
 
-var createGame = () => {
-	const gamePlay = new GamePlay();
+var createGame = (userWallet) => {
+	joinGame(userWallet);
+}
+
+var joinGame = (userWallet) => {
+	var gamePlay = new GamePlay(userWallet);
 	gamePlay.start();
 }
 
-var joinGame = () => {
-
+var start = (userWallet) => {
+	var menu = new Menu();
+	menu.showMainMenu(
+		() => createGame(userWallet), 
+		() => joinGame(userWallet)
+	);
 }
 
-const menu = new Menu();
-menu.showMainMenu(createGame, joinGame);
+var startAsPlayer = () => {
+	var getArgPlayer = () => process.argv.length > 2 ? process.argv[2] : null;
+	var getPlayer = (player) => defaultWallets.find((n) => n.name === player);
+	var getPlayerVal = (label) => label.replace(" ", "").toLowerCase();
+	
+	var userWallet = getPlayer(getArgPlayer());
+	if (userWallet != null) {
+		start(userWallet);
+	} else {
+		inquirer
+		.prompt([ { type: 'list', name: 'player', message: 'Select player', choices: ['Player 1', 'Player 2'] } ])
+		.then((answers) => {
+			userWallet = getPlayer(getPlayerVal(answers.player));
+			start(userWallet);
+		});
+	}
+}
+
+startAsPlayer();
 
 
 // const greeting = white.bgGreen.bold("Hola mundo!");
