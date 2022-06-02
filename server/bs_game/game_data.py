5d68b74d3692cbb5bcb5ba919ec36a4f6cc080f7
lachezar.gorchev <lachezar.gorchev@bgosoftware.com>
2022-06-02 18:05:35 +0300

    Advanced game initialization. Support for invites. Additional checks for None.

--- server/bs_game/game_data.py
+++ server/bs_game/game_data.py
@@ -14,11 +14,13 @@ class BSGameRules:
 
 class BSPlayer:
 	Id = ""
+	tag = 0
 	keyCrypt = None
 	boardCrypt = None
 	board = None
-	def __init__(self, _gameRules, Id):
+	def __init__(self, _gameRules, Id, tag):
 		self.Id = str(Id).lower()
+		self.tag = tag
 		self.board = [ [ 0 for ix in range(_gameRules.boardSizeX) ] for iy in range(_gameRules.boardSizeY) ]
 
 class BSGameMove:
@@ -29,9 +31,37 @@ class BSGameMove:
 	def __init__(self, player, wasHit, mx, my):
 		self.player = convertToInt(player) ; self.wasHit = convertToInt(wasHit) ; self.mx = convertToInt(mx) ; self.my = convertToInt(my)
 
-class BSGameState:
-	_gameRules = None
+class GameDescriptor:
 	gameId = ""
+	owner = ""
+	tokenCreate = ""
+	invite = []
+
+	def __init__(self, gameId, owner, tokenCreate, invite):
+		self.gameId = str(gameId)
+		self.owner = str(owner)
+		self.tokenCreate = str(tokenCreate)
+		self.invite = invite
+
+class BSGameStateBase:
+	def getGameId(self): return self.descriptor.gameId
+	def getGameOwner(self): return self.descriptor.owner
+	def getGameTokenCreate(self): return self.descriptor.tokenCreate
+ 
+	def isUserOwner(self, address): return self.getGameOwner() == address
+
+	def isUserInvited(self, address):
+		return len(self.descriptor.invite) == 0 or str(address).lower() in self.descriptor.invite
+
+	def isUserAllowed(self, address):
+		return self.isUserOwner(address) or self.isUserInvited(address)
+
+	def __init__(self, descriptor):
+		self.descriptor = descriptor
+		self.gameId = descriptor.gameId
+
+class BSGameState(BSGameStateBase):
+	_gameRules = None
 	player1 = None
 	player2 = None
 	moveHistory = []
@@ -39,17 +69,22 @@ class BSGameState:
 	result = 0 # 0 - to be decided, 1 - player 1 is winner, 2 - player 2 is winner
 	# TODO: status for cheating
 	
-	def __init__(self, _gameRules, gameId):
+	def __init__(self, descriptor, _gameRules):
+		super().__init__(descriptor)
 		self._gameRules = _gameRules
-		self.gameId = str(gameId)
 
-	def addPlayer(self, id):
-		if self.player1 is None:
-			self.player1 = BSPlayer(self._gameRules, id)
-			return
-		if self.player2 is None:
-			self.player2 = BSPlayer(self._gameRules, id)
-			return
+	def addPlayer(self, address, board):
+		# game owner always joins as Player 1
+		if self.player1 is None and self.isUserOwner(address):
+			self.player1 = BSPlayer(self._gameRules, address, 1)
+			self.player1.boardCrypt = board
+			return self.player1
+		# anyone of the invited players (except the owner) can join as Player 2
+		if self.player2 is None and self.isUserInvited(address) and not self.isUserOwner(address):
+			self.player2 = BSPlayer(self._gameRules, address, 2)
+			self.player2.boardCrypt = board
+			return self.player2
+		return None
 	
 	def getPlayerByTag(self, playerTag):
 		if playerTag == 1:
@@ -58,8 +93,8 @@ class BSGameState:
 			return self.player2
 	
 	def getPlayerTagById(self, id):
-		if self.player1.Id == id: return 1
-		if self.player2.Id == id: return 2
+		if not self.player1 is None and self.player1.Id == id: return 1
+		if not self.player2 is None and self.player2.Id == id: return 2
 		return 0
 	
 	def getOpponentTag(self, player):
