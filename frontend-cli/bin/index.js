fd44d21970346789cf89b85357502f3d84f7cd71
lachezar.gorchev <lachezar.gorchev@bgosoftware.com>
2022-06-22 14:40:04 +0300

    Flow for creating/joining game.

--- frontend-cli/bin/index.js
+++ frontend-cli/bin/index.js
@@ -5,28 +5,61 @@ import { defaultWallets } from "../src/connect/wallets"
 import Menu from "../src/menu.js";
 import GameManager from "../src/game-manage.js";
 import GamePlay from "../src/game-play.js";
+import { Receiver } from "../src/connect/receive"
+import { BSGameRules, BSGameState, BSGameMove } from "../src/game-data.js";
 
 const menu = new Menu();
 
 var createGame = (userWallet) => {
-	menu.showCreateGameMenu((answers) => {
-		var gameMgr = new GameManager(userWallet);
-		gameMgr.gameCreate(answers);
-		joinGame(userWallet);
+	menu.showCreateGameMenu((answer_GameCreate) => {
+		if (answer_GameCreate.confirm) {
+			var gameMgr = new GameManager(userWallet);
+			gameMgr.gameCreate(answer_GameCreate, (gameState) => {
+				menu.askConfirm("Join now?", (answer_Join) => {
+					if (answer_Join.confirm) {
+						console.log("Waiting players to join ...");
+						gameMgr.gameJoin(gameState, (player, isGameReadyToStart) => {
+							if (player != null) {
+								console.log("User with address {#a} joined as Player {#p}.".replace("{#p}", player.playerTag).replace("{#a}", player.playerId));
+								if (isGameReadyToStart) {
+									menu.askConfirm("Game is ready. Start now?", (answer_Start) => {
+										if (answer_Start.confirm) {
+											var gamePlay = new GamePlay(gameState, userWallet);
+											gamePlay.start();		
+										}
+									});
+								}
+							}
+						});
+					}
+				});
+			});
+		}
 	});
 }
 
 var joinGame = (userWallet) => {
 	menu.showJoinGameMenu((answers) => {
-		var gamePlay = new GamePlay(answers.gameId, userWallet);
+		var gameRules = new BSGameRules();
+		var gameState = new BSGameState(gameRules, answers.gameId, getWalletAddress(userWallet));
+		var gamePlay = new GamePlay(gameState, userWallet);
 		gamePlay.start();
 	});
+}
+
+var testReveive = (userWallet) => {
+	let rcv = new Receiver(userWallet, (messages) => {
+		if (messages.length > 0) {
+			console.log(messages);
+		}
+	}).startListen();
 }
 
 var start = (userWallet) => {
 	menu.showMainMenu(
 		() => createGame(userWallet), 
-		() => joinGame(userWallet)
+		() => joinGame(userWallet),
+		() => testReveive(userWallet)
 	);
 }
 
