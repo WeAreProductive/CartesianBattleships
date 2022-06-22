9267c4a487216d6d850940043452eee79923c3bf
lachezar.gorchev <lachezar.gorchev@bgosoftware.com>
2022-06-22 14:37:33 +0300

    Manager for creating and joining game.

--- frontend-cli/src/game-manage.js
+++ frontend-cli/src/game-manage.js
@@ -1,25 +1,60 @@
 import GameProtocol from "./game-protocol.js";
 import { sendCommand, getWalletAddress } from "./connect/send";
+import { Receiver } from "./connect/receive"
 
 export default function GameManager(userWallet) {
 
-	var protocol = new GameProtocol();
+	const protocol = new GameProtocol();
 
-	var generateRandomHexString = () => {
-		return Math.random().toString(16).replace(/[^a-f\d]+/g, '');		
-	}
+	const generateRandomHexString = () => Math.random().toString(16).replace(/[^a-f\d]+/g, '');
 
-	this.gameCreate = (answers) => {
-		var reqId = generateRandomHexString();
+	this.gameCreate = (answers, onCreate, onError) => {
+		let reqId = generateRandomHexString();
 
-		var invite = answers.visibility == "2" ? (answers.address + "").split(",") : [];
+		let invite = answers.visibility == "2" ? (answers.address + "").split(",") : [];
 		invite = invite.map(a => a.trim().toLowerCase());
 
-		var sz = answers.rules == "std" ? 10 : Number(answers.boardsize);
-		var boardSize = !isNaN(sz) ? [sz, sz] : [Number(answers.bsx), Number(answers.bsy)];
+		let sz = answers.rules == "std" ? 10 : Number(answers.boardsize);
+		let boardSize = !isNaN(sz) ? [sz, sz] : [Number(answers.bsx), Number(answers.bsy)];
 	
-		var msg = protocol.createCommand_gameCreate(reqId, invite, boardSize[0], boardSize[1]);
+		// prepare and send command
+		let msg = protocol.createCommand_gameCreate(reqId, invite, boardSize[0], boardSize[1]);
 		sendCommand(userWallet, msg);
+		
+
+		// wait for response
+		console.log("Creating game ... (please wait)");
+		let rcv = new Receiver(userWallet)
+			.filter((n) => n.sys != null && n.sys.reqId == reqId) 
+			.onReceive((messages) => {
+				rcv.stopListen();
+				let gameState = protocol.parseResponse_gameCreate(messages[0], getWalletAddress(userWallet));
+				console.log("Game created (Game ID: {#gid})".replace("{#gid}", gameState.gameId));
+				onCreate(gameState);
+			})
+			.startListen();
+	}
+
+	this.gameJoin = (gameState, onJoin) => {
+		// prepare and send command
+		let msg = protocol.createCommand_gameJoin(gameState.gameId, "****");
+		sendCommand(userWallet, msg);
+
+		// wait for response
+		let rcv = new Receiver(userWallet)
+			.filter((n) => n.gid == gameState.gameId && n.cmd == "j")
+			.onReceive((messages) => {
+				messages.forEach(msg => {
+					let join = protocol.parseResponse_gameJoin(msg, gameState);
+					console.log(join);
+					if (join.isGameReadyToStart) {
+						rcv.stopListen();
+					}
+					onJoin(join.player, join.isGameReadyToStart);
+				});
+			})
+			.startListen();
+
 	}
 
 }
