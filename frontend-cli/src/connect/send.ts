2f2f1a940135360eee61478ee45ab783795c3999
lachezar.gorchev <lachezar.gorchev@bgosoftware.com>
2022-06-22 14:36:54 +0300

    Epoch/Input indices returned when sending.

--- frontend-cli/src/connect/send.ts
+++ frontend-cli/src/connect/send.ts
@@ -1,6 +1,25 @@
-import { ethers } from "ethers";
+import { ethers, ContractReceipt } from "ethers";
 import { connect } from "./connect";
 import { UserWallet } from "./wallets"
+import { NoticeKeys } from "../../generated-src/graphql";
+import { InputAddedEvent } from "../../generated-src/rollups/Input";
+
+const findNoticeKeys = (receipt: ContractReceipt): NoticeKeys => {
+    // get InputAddedEvent from transaction receipt
+    const event = receipt.events?.find((e) => e.event === "InputAdded");
+
+    if (!event) {
+		return { epoch_index: "", input_index: "" };
+        //throw new Error(`InputAdded event not found in receipt of transaction ${receipt.transactionHash}`);
+    }
+
+    const inputAdded = event as InputAddedEvent;
+    return {
+        epoch_index: inputAdded.args._epochNumber.toString(),
+		input_index: event !=null && event.args != null ? event.args._inputIndex.toString() : undefined,
+        // XXX: input_index: event.args._inputIndex.toString(),
+    };
+};
 
 export const sendCommand = async (userWallet: UserWallet, message: string) => {
 	const { chain, inputContract } = await connect(userWallet.network, userWallet.mnemonic, userWallet.path);
@@ -8,6 +27,9 @@ export const sendCommand = async (userWallet: UserWallet, message: string) => {
 
 	const tx = await inputContract.addInput(input);
 	const receipt = await tx.wait(1);
+	const noticeKeys = findNoticeKeys(receipt);
+
+	return noticeKeys;
 }
 
 export const getWalletAddress = (userWallet: UserWallet): string => {
