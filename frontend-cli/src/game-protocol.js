7737397efd441bbb3e3f7b61a4db67f5fde01159
lachezar.gorchev <lachezar.gorchev@bgosoftware.com>
2022-06-22 14:38:48 +0300

    Game protocol parses responses.

--- frontend-cli/src/game-protocol.js
+++ frontend-cli/src/game-protocol.js
@@ -1,4 +1,4 @@
-//import { BSGameRules, BSGameState, BSGameMove } from "./game-data.js";
+import { BSGameRules, BSGameState, BSGameMove } from "./game-data.js";
 
 export default function GameProtocol() {
 
@@ -43,6 +43,29 @@ export default function GameProtocol() {
 		};
 		return JSON.stringify(cmd);		
 		//return "e: " + key;
+	}
+
+	this.parseResponse_gameCreate = (cmd, userAddress) => {
+		var gameRules = new BSGameRules();
+		gameRules.boardX = parseInt(cmd.arg.rules.size[0]);
+		gameRules.boardY = parseInt(cmd.arg.rules.size[1]);
+
+		var gameState = new BSGameState(gameRules, cmd.gid, userAddress);
+		gameState.owner = cmd.sys.owner;
+		gameState.invite = cmd.sys.invite;
+	
+		return gameState;
+	}
+
+	this.parseResponse_gameJoin = (cmd, gameState) => {
+		// TODO: for now empty invite (meaning "anyone") will not work, player 2 must be invited with address
+		let sender = ""; // TODO: playerId should be taken from sender
+		let playerTag = parseInt(cmd.p);
+		if (playerTag == 1) sender = gameState.owner;
+		if (playerTag == 2) sender = gameState.invite.length > 0 ? gameState.invite[0] : "";
+		let player = gameState.addPlayer(playerTag, sender, cmd.arg.board);
+		let isGameReadyToStart = gameState.isReadyToStart();
+		return { player: player, isGameReadyToStart: isGameReadyToStart };
 	}
 
 }
