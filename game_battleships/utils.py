7afca02f8a6a30c5305de9fca678ce6a917417a1
lachezar.gorchev <lachezar.gorchev@bgosoftware.com>
2022-07-05 01:22:48 +0300

    Added default value None.

--- game_battleships/utils.py
+++ game_battleships/utils.py
@@ -7,14 +7,14 @@ def convertToInt(val, defaultVal = 0):
 def convertHexBytesToString(text):
 	return bytes.fromhex(text[2:]).decode("utf-8")
 	#return bytearray.fromhex(str(text).replace("0x", "")).decode()
-	 
+
 def convertStringToHexBytes(text):
 	return "0x" + str(text).encode("utf-8").hex()
 	#return "0x" + bytearray(text, "ascii").hex()
 
-def getKeySafe(data, key, defaultVal):
+def getKeySafe(data, key, defaultVal = None):
 	return data[key] if not data is None and key in data else defaultVal
 
-def ensureKey(data, key, defaultVal):
+def ensureKey(data, key, defaultVal = None):
 	if not data is None and not key in data:
 		data[key] = defaultVal
