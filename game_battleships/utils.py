90eb5ffe83afb6f03d1bb3674b2b64b556e9e8a1
lachezar.gorchev <lachezar.gorchev@bgosoftware.com>
2022-07-18 17:04:30 +0300

    Method "getKeySafe" extended to support nested keys.

--- game_battleships/utils.py
+++ game_battleships/utils.py
@@ -12,7 +12,18 @@ def convertStringToHexBytes(text):
 	return "0x" + str(text).encode("utf-8").hex()
 	#return "0x" + bytearray(text, "ascii").hex()
 
+# get element from dictionary without errors
+# key can be string to access the item directly
+# or a list of strings to access the item if nested in structure of inner dictionaries
+# if item not found then defaultVal will be returned
 def getKeySafe(data, key, defaultVal = None):
+	if isinstance(key, list):
+		if len(key) > 1:
+			data = getKeySafe(data, key[0], defaultVal)
+			del key[0]
+		else:
+			key = key[0]
+		return getKeySafe(data, key, defaultVal)
 	return data[key] if not data is None and key in data else defaultVal
 
 def ensureKey(data, key, defaultVal = None):
